56a19edce4b075f87ea4985223af09c1a46a5bd6
ngoc <tongoc92@gmail.com>
2017-05-10 13:44:19 +0700

    short function syntax instead

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -7,29 +7,29 @@ describe('Calculator test suite', () => {
     
     let calculator = null;
     
-    beforeEach(function () {
+    beforeEach(() => {
         calculator = new Calculator();
     });
 
-    it('can do add', function () {
+    it('can do add', () => {
         let result = calculator.add(1, 2);
 
         assert.equal(3, result);
     });
 
-    it('can do substract', function () {
+    it('can do substract', () => {
         let result = calculator.substract(2, 1);
 
         assert.equal(1, result);
     });
 
-    it('can do multiply', function () {
+    it('can do multiply', () => {
         let result = calculator.multiply(2, 3);
 
         assert.equal(6, result);
     });
 
-    it('can do divide', function () {
+    it('can do divide', () => {
         let result = calculator.divide(6, 3);
 
         assert.equal(2, result);
