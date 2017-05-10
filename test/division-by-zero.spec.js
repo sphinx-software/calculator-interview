a1c4f10e76536693ba02e8dad278d0f6a76b7db0
Rikky <son.levuthai@gmail.com>
2017-05-10 15:55:24 +0700

    add test division by zero separately

---
+++ test/division-by-zero.spec.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const Calculator = require('./../src/calculator');
+
+describe('Calculator division by zero', () => {
+
+    let calculator = null;
+
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
+
+    it('must throw division by zero exception when it does', () => {
+        // TODO your code here!
+        throw new Error('You should implement this test');
+    });
+
+});
