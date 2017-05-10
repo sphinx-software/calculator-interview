da12499a97bd9b7be2321ed009722c299d811ea0
ngoc <tongoc92@gmail.com>
2017-05-10 13:57:09 +0700

    add emoji ;)

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -34,4 +34,8 @@ describe('Calculator test suite', () => {
 
         assert.equal(2, result);
     });
+
+    it('must throw division by zero exception when it does', () => {
+        // TODO your code here!
+    })
 });
